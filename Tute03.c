e478e73a8ec08f087fc47f0428bbef270cc56551
IT21349874 <99617194+IT21349874@users.noreply.github.com>
2022-03-07 16:58:07 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -11,9 +11,17 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
-int main() {
-  
-  return 0;
+#include<stdio.h>
+int main(void){
+	int x,count,ans=0;
+	
+	printf("This is an arithmetic sequence of 1+2+3+.....+x\n Enter x :");
+	scanf("%d", &x);
+	
+	for(count=1; count<=x; count++){
+	ans+=count;	
+	}
+	printf("The answer is : %d", ans);
+	return 0;
 }
 
